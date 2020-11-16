24f8ffd29c58a8b00e3b3a75b3bd6a3432415433
Gadi Eichhorn <gadi.eichhorn@gmail.com>
2020-11-16 11:39:30 +0000

    fixed random and generated a full cycle period calculation
    Signed-off-by: Gadi Eichhorn <gadi.eichhorn@gmail.com>

--- main.py
+++ main.py
@@ -1,26 +1,37 @@
+import random
+
 if __name__ == '__main__':
+
+    N = 20
 
     truth = [0, 1, 1, 1, 0, 1, 1, 0]
 
     state = []
 
-    for i in range(20):
-        state.append(1)
+    for i in range(N):
+        r = random.randint(0, 1)
+        state.append(r)
 
     print("Truth", truth)
     print("State", state)
 
     connections = []
 
-    for i in range(20):
-        connections.append([1, 2, 12])
+    for i in range(N):
+        connections.append([random.randint(0, N - 1), random.randint(0, N - 1), random.randint(0, N - 1)])
+
+    print("Connections", connections)
 
     state2 = []
 
-    for j in len(state):
-        a = connections[j][0]
-        b = connections[j][1]
-        c = connections[j][2]
-        print("Nodes", a, b, c)
+    for j in range(N):
+        a = state[connections[j][0]]
+        b = state[connections[j][1]]
+        c = state[connections[j][2]]
+        num = a * 4 + b * 2 + c
+        print("Nodes", a, b, c, num)
+        state2.append(truth[num])
+        print("State'", truth[num])
 
-    print("Connections", connections)
+    state = state2
+    print("State", state)
